33dfb4cbd630885ea0bd586ba2b32f0625cc91f4
Ali Husnain <pkihusnain220@gmail.com>
2023-05-15 10:23:25 +0500

    gulp purgecss compress styleSheet

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,3 @@
-include npm dev dependencies terminal | cmd -> npm i gulp gulp-sass sass --save-div
+include npm dev dependencies terminal |
+cmd :-> npm i gulp gulp-sass sass --save-div 
+    :-> npm instal gulp-purgecss --save-dev
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 const buildStyles = () => {
   return src('shinabi/**/*.scss')
     .pipe(sass())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 const watchTask = () => {
-  watch(['shinabi/**/*.scss'], buildStyles);
+  watch(['shinabi/**/*.scss','*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
